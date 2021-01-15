e3121ddf5a36190f6162b9c56540ae76331705f3
John <dsadsa@aolc.com>
2021-01-15 19:25:47 +0100

    better 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,6 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import logo from "./logo.jpg"
 
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
@@ -11,7 +10,6 @@ const NotFoundPage = ({ data, location }) => {
   return (
     <Layout location={location} >
       <SEO title="404: Not Found" />
-      <center><img src={logo} alt="Logo"/></center>
       <h1>404: Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Layout>
